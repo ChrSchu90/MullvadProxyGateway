653c57e0f8176e28cf5d4059441fa4a60d2543a2
ChrSchu <chr.schu@outlook.com>
2026-03-13 17:19:47 +0100

    Update debug script

--- debug.sh
+++ debug.sh
@@ -6,12 +6,12 @@ DOCKER_FILE="Dockerfile"
 IMAGE_NAME="mullvad-proxy-gateway:dev"
 NET_PROJECT=./GostGen/source/GostGen.csproj
 NET_BUILD_ARGS="-p:DebugType=embedded -p:PublishSingleFile=true -p:Version=0.0.1 --verbosity normal --configuration Release --self-contained"
+DOCKER_PLATFORM=linux/amd64 # linux/amd64 linux/arm64/v8 linux/arm/v7
 GOST_VERSION="3.2.6"
 
 dotnet publish ${NET_PROJECT} -r linux-musl-x64 ${NET_BUILD_ARGS} -o ./GostGen/publish/linux/amd64 && \
   dotnet publish ${NET_PROJECT} -r linux-musl-arm64 ${NET_BUILD_ARGS} -o ./GostGen/publish/linux/arm64 && \
   dotnet publish ${NET_PROJECT} -r linux-musl-arm ${NET_BUILD_ARGS} -o ./GostGen/publish/linux/armv7 && \
-  #docker buildx build --progress=plain --platform linux/amd64,linux/arm64/v8,linux/arm/v7 --build-arg GOST_VERSION=${GOST_VERSION} -f ${DOCKER_FILE} -t ${IMAGE_NAME} . && \
-  docker buildx build --progress=plain --platform linux/amd64 --build-arg GOST_VERSION=${GOST_VERSION} -f ${DOCKER_FILE} -t ${IMAGE_NAME} . && \
+  docker buildx build --load --progress=plain --platform ${DOCKER_PLATFORM} --build-arg GOST_VERSION=${GOST_VERSION} -f ${DOCKER_FILE} -t ${IMAGE_NAME} . && \
   docker volume create mullvadproxygateway_data  && \
-  docker run --rm -it -v mullvadproxygateway_data:/data -p 1080:1080 -p 9100:9100 -p 2000-5000:2000-5000 --cap-add NET_ADMIN --sysctl net.ipv4.conf.all.src_valid_mark=1 ${IMAGE_NAME}
+  docker run --rm -it --platform ${DOCKER_PLATFORM} -v mullvadproxygateway_data:/data -p 1080:1080 -p 9100:9100 -p 2000-2050:2000-2050 --cap-add NET_ADMIN --sysctl net.ipv4.conf.all.src_valid_mark=1 ${IMAGE_NAME}
